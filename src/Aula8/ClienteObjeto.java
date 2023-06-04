edc6ba5c52f6a0eb800cdf577284eeafdfd4d278
karensantana11 <133985203+karensantana11@users.noreply.github.com>
2023-06-04 18:54:25 -0300

    Aula8

---
+++ src/Aula8/ClienteObjeto.java
@@ -0,0 +1,24 @@
+package Aula8;
+
+import java.util.Scanner;
+
+    public class ClienteObjeto {
+	public static void main(String[] args) {
+		
+		
+	Scanner leia = new Scanner (System.in);
+
+    Cliente cliente1 = new Cliente (4075, 1998283,"CC", "Elisangela", 40.000f);
+	
+    cliente1.visualizar();
+	
+	
+	
+	
+	
+	
+	
+	
+    }
+	
+}
